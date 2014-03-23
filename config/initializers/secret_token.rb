6d87a1614993971d9cfc30df1fbc4f6a0e31bfaa
Alcemir Sardo <alcemir.sardo@gmail.com>
2014-03-23 03:24:26 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sardoapp001::Application.config.secret_key_base = '80da68fbdd89b30216b93266c4d6ce1507670bcd4a3ffc5cbee203c884d33227c810d6202a19e9c67cde0406e825cc309787ec088d263fc09bd967ba9c90ff2f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sardoapp001::Application.config.session_store :cookie_store, key: '_sardoapp-001_session'
